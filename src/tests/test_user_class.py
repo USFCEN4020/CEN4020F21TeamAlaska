7ca6f9c98c7e6e9d35b0fdbdab6ac2a2c45707f7
Daniel Arvelo <darvelo00@gmail.com>
2021-09-29 22:32:32 -0400

    update old test with new output

--- src/tests/test_user_class.py
+++ src/tests/test_user_class.py
@@ -166,11 +166,11 @@ class TestRegisterLogin:
         self.db.print_users()
         src.database_access.print = print
         assert output == [
-            ('randion', 'Password#1', 'Robby', 'Ybbor'),
-            ('randion0', 'Password#10', 'Robby0', 'Ybbor0'),
-            ('randion1', 'Password#11', 'Robby1', 'Ybbor1'),
-            ('randion2', 'Password#12', 'Robby2', 'Ybbor2'),
-            ('randion3', 'Password#13', 'Robby3', 'Ybbor3')
+            ('randion', 'Password#1', 'Robby', 'Ybbor', True, True, True),
+            ('randion0', 'Password#10', 'Robby0', 'Ybbor0', True, True, True),
+            ('randion1', 'Password#11', 'Robby1', 'Ybbor1', True, True, True),
+            ('randion2', 'Password#12', 'Robby2', 'Ybbor2', True, True, True),
+            ('randion3', 'Password#13', 'Robby3', 'Ybbor3', True, True, True)
         ]
 
     def testCleanUp(self):  # Teardown
