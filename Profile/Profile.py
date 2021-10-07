78ae1d0aa5af79e795eb19a77627b2be83f5abab
Daniel Arvelo <darvelo00@gmail.com>
2021-10-06 21:54:33 -0400

    added Profile class and instantiating function

--- Profile/Profile.py
+++ Profile/Profile.py
@@ -1,25 +1,56 @@
-def printUserProfile(user, db): # Requires Database and User object, will print out full profile.
+from src.User import User
+from src.database_access import database_access
+
+
+class Profile:
+    def __init__(self, username: str, title: str, major: str, university_name: str, about_me: str, education: str):
+        self.username = username,
+        self.title = title
+        self.major = major
+        self.university_name = university_name
+        self.about_me = about_me
+        self.education = education
+
+
+def getProfile(username: str, db: database_access) -> Profile:
     profileQueryString = '''
     SELECT *
     FROM profile
     WHERE username = ?
     '''
+    profileInformation = db.execute(profileQueryString, [username])
+    if profileInformation:
+        return Profile(
+            username,
+            profileInformation[0][1],
+            profileInformation[0][2],
+            profileInformation[0][3],
+            profileInformation[0][4],
+            profileInformation[0][5],
+        )
+    else:
+        return Profile("", "", "", "", "", "")
+
+
+# Requires Database and User object, will print out full profile.
+def printUserProfile(user: User, db: database_access):
     jobQueryString = '''
     SELECT *
     FROM job_experience
     WHERE username = ?
     '''
-    profileInformation = db.execute(profileQueryString, [user.username])
+    profileInformation = getProfile(user.username, db)
     jobInformation = db.execute(jobQueryString, [user.username])
     if len(profileInformation) < 1:
         return -1
 
     print_queue = []
     print_queue.append(user.firstname + ' ' + user.lastname + '\'s Profile')
-    print_queue.append('Title: ' + profileInformation[0][0])
-    print_queue.append('Major: ' + profileInformation[0][1])
-    print_queue.append('University: ' + profileInformation[0][2])
-    print_queue.append('Information and Education\n' + profileInformation[0][3] + ' ' + profileInformation[0][4])
+    print_queue.append('Title: ' + profileInformation.title)
+    print_queue.append('Major: ' + profileInformation.major)
+    print_queue.append('University: ' + profileInformation.university_name)
+    print_queue.append('Information and Education\n' +
+                       profileInformation.about_me + ' ' + profileInformation.education)
 
     if len(jobInformation) > 0:
         print_queue.append('Job Experience')
@@ -33,4 +64,3 @@ def printUserProfile(user, db): # Requires Database and User object, will print 
 
     for item in print_queue:
         print(item + '\n')
-
