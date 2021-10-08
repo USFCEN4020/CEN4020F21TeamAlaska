d81d004c59ea84d46b79dc6a1444ccba4be7896b
Daniel Arvelo <darvelo00@gmail.com>
2021-10-07 21:14:45 -0400

    Added editProfile page and moved profile pages to Page class

--- src/index.py
+++ src/index.py
@@ -4,7 +4,7 @@ from src.database_access import database_access as Database
 
 # ================ MAIN ==================
 # you can think of it as the driver function. It's where everything starts
-def main(db: Database):
+def main():
     page = Page()
     page.home_page()
 
@@ -12,5 +12,5 @@ def main(db: Database):
 if __name__ == "__main__":
     # Making a connection to the database. If it doesn't already exist, it creates it
     db = Database("InCollege.sqlite3")
-    main(db)
+    main()
     db.close()
