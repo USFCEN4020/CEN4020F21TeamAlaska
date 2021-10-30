775a2cf90ba049d73f856a3d7ccf178b72bbd6cb
Robert <randion@usf.edu>
2021-10-30 13:31:27 -0400

    Start of test

--- src/tests/test_messages.py
+++ src/tests/test_messages.py
@@ -10,13 +10,10 @@ import src.message
 # Does initial setup before any test runs
 
 def setup_module():
-    global user
     global db
     global message
     message = Message()
     db = Database("testing.sqlite3-messages")
-    user = User("", "", "", "", "Standard", "english",
-        True, True, True, False, db)
     src.Page.db = db
     db.delete_profile_table()
     db.delete_users_table()
@@ -27,11 +24,27 @@ def setup_module():
 
 
 class TestMessages:
+    u1 = create_user(('randion','Password#1','Robert',"Tester", "standard"), db)
+    u2 = create_user(('randionSquared','Password#1','Robert',"SquaredGfuelAddict", "plus"), db)
+
+    def testSendMessageNonFriendPlus(self):
+        page = src.Page.Page()
+        page.user = self.u2
+        src.Page.back_option = lambda s: output.append('Done')
+        page.back_option = lambda s: output.append('Done')
+        output = []
+        input = ["2","1","1"]
+        def mock_input(s):
+            return input.pop(0)
+        src.Page.print = lambda s: output.append(s)
+        src.Page.input = mock_input
+        src.helpers.input = mock_input
+        page.messages_page()
+        
+        assert output == []
+    #        message.send_message('randion','randionSquared', "I love to test", db)
     def testSendMessage(self):
-        #user.createUser(['randion','Password#1','Robert',"Tester", "Standard"], db)
-        #user.createUser(['randionSquared','Password#1','Robert',"SquaredGfuelAddict", "Plus"], db)
-        #message.send_message('randion','randionSquared', "I love to test", db)
-        assert 'please' == 'please'
+        print('qait')
 
 def teardown_module():
     db = Database('testing.sqlite3-messages')
@@ -42,4 +55,5 @@ def teardown_module():
     db.delete_jobs_table()
     db.delete_user_applied()
     db.delete_user_interested()
+    db.delete_messages()
     db.close()
